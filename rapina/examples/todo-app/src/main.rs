1fad623c80c97356f490d782a235433549a443ff
apetrovic <petrovicante6@gmail.com>
2026-04-18 12:12:12 +0200

    update: add missing config QOL features to the todo-app
    - rename the db in the .env.example
    - add database_url string in the AppConfig struct, parse it out and pass
    it into the .with_database config call
    - improve the error clarity for the end user when constructing the
    AppConfig

--- rapina/examples/todo-app/src/main.rs
+++ rapina/examples/todo-app/src/main.rs
@@ -19,11 +19,18 @@ pub struct AppConfig {
     #[env = "MAX_TODOS"]
     #[default = "5"]
     max_todos: u32,
+
+    #[env = "DATABASE_URL"]
+    #[default = "sqlite://todos.db?mode=rwc"]
+    db_url: String,
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let config = AppConfig::from_env().expect("Missing Config");
+    let config = AppConfig::from_env()
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidInput, e.to_string()))?;
+
+    let db_url = config.db_url.clone();
     let addr = format!("{}:{}", config.host, config.port);
 
     Rapina::new()
@@ -31,7 +38,7 @@ async fn main() -> std::io::Result<()> {
         .state(config)
         .openapi("Todo API", "1.0.0")
         .middleware(RequestLogMiddleware::new())
-        .with_database(DatabaseConfig::new("sqlite://todos.db?mode=rwc"))
+        .with_database(DatabaseConfig::new(db_url))
         .await?
         .run_migrations::<migrations::Migrator>()
         .await?
